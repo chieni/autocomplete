4313ae7ebdc39e9426eef6b75e2abac7518a2542
chieni <mitchieni@gmail.com>
2015-09-23 02:21:26 -0400

    added comments

--- tests.js
+++ tests.js
@@ -1,3 +1,8 @@
 QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+	var a = ["a", "to", "tea", "ted", "teds", "ten", "tennis", "i", "in", "inn"];
+	var t = TrieNode(null);
+	a.forEach(function(w){t.insert(w);});
+	var matchList = t.getMatchList("t", 10);
+
+  	assert.ok( matchList[0] == "tea", "Passed!" );
 });
--- trie.js
+++ trie.js
@@ -1,11 +1,24 @@
+/**
+Create a TrieNode object.
+A TrieNode object represents a node in a Trie data structure. Each node contains a key
+and a value.
+
+@constructor
+@param {String} key - the key of the TrieNode, i.e. the word or word part that it represents
+@param {int} value - the value of the TrieNode, 1 if it is a word 
+in the dictionary and 0 if it is merely a word part
+
+**/
 var TrieNode = function(key, value) {
 	var that = Object.create(TrieNode.prototype);
 	var children = [];
 
-	/*
-	Walk through the trie according to the string to be inserted, then append
-	new nodes for the string that is not contained in the trie.
-	*/
+	/**
+	Inserts a TrieNode into an existing Trie data structure (consisting of TrieNodes)
+	@param {String} word - the word being inserted
+	@param {int} depth - the depth of the insertion. This value is used in recursion, but is an 
+	optional parameter
+	**/
 	that.insert = function(word, depth){
 		if (depth == null){depth = 0;}
 
@@ -31,9 +44,13 @@ var TrieNode = function(key, value) {
 		}
 	}
 
-	/*
-	Returns the node where a word terminates
-	*/
+	/**
+	Finds and returns the node that matches the word parameter. If none matches,
+	returns null.
+	@param {String} word - the word being searched for
+	@depth {int} depth - the depth of the recursion. This is an optional parameter
+	that is not used in the public space.
+	**/
 	that.find = function(word, depth){
 		if (depth == null){depth = 0;}
 		// Search through the children nodes
@@ -55,15 +72,19 @@ var TrieNode = function(key, value) {
 		}
 	}
 
-	/*
-	Find the correct node in the Trie structure, then use preorder traversal on it.
-	*/
-	that.getMatchList = function(word){
-		var matchNode = this.find(word);
+	/**
+	Returns an array of strings, at most a certain length, that represent the
+	list of autocomplete words available. If there are no matches, returns null.
+	@param {String} word - the word being autocompleted upon
+	@param {int} maxLength - the maximum number of words to be shown in autocomplete. 
+	Represents the maximum length of the array that is returned.
+	**/
+	that.getMatchList = function(word, maxLength){
+		var matchNode = that.find(word);
 		if (matchNode){
 			var matchChildren = matchNode.preOrderTraversal();
-			if (matchChildren.length > 10){
-				return matchChildren.slice(0, 10);
+			if (matchChildren.length > maxLength){
+				return matchChildren.slice(0, maxLength);
 			}
 			return matchChildren;
 		} else {
@@ -71,10 +92,9 @@ var TrieNode = function(key, value) {
 		}
 	}
 
-	/*
-	If node has a value, add to list. Sort children by alphabetical order. 
-	Call preOrderTraversal on the children
-	*/
+	/**
+	Returns a list of nodes that were discovered in preorder traversal. 
+	**/
 	that.preOrderTraversal = function(){
 		var visited = [];
 		if (value) {
@@ -85,6 +105,13 @@ var TrieNode = function(key, value) {
 			return a.key().localeCompare(b.key());
 		});
 
+		/* EXAMPLE USE OF FUNCTIONALS
+			This is my most notable use of functionals. I use the forEach functional to loop
+			over the children of the TrieNode and call preOrderTraversal recursively on them
+			in order to do a preorder traversal of the Trie. All this functionality is 
+			condensed to what is essentially a single line of code, which makes it clean and 
+			easy to understand. 
+		*/
 		children.forEach(function(child){
 			visited.push.apply(visited, child.preOrderTraversal());
 		});
@@ -92,12 +119,11 @@ var TrieNode = function(key, value) {
 		return visited;
 	}
 
+	/**
+	Returns the key of the TrieNode. 
+	**/
 	that.key = function(){
 		return key;
-	}
-
-	that.value = function(){
-		return value;
 	}
 
 	Object.freeze(that);
@@ -108,4 +134,4 @@ var TrieNode = function(key, value) {
 var a = ["a", "to", "tea", "ted", "teds", "ten", "tennis", "i", "in", "inn"];
 var t = TrieNode(null);
 a.forEach(function(w){t.insert(w);});
-console.log(t.getMatchList("t"));
+console.log(t.getMatchList("t", 10));
